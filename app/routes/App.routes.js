23f044e2d2e9a33becb5be000e653cdecc3caa30
rianabd01 <rianwahyuab@gmail.com>
2024-06-29 22:23:52 +0700

    Add: login and add to cart routes

--- app/routes/App.routes.js
+++ app/routes/App.routes.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const { getProducts } = require('../controllers/productController');
+const getProducts = require('../controllers/productController');
+const login = require('../controllers/Auth/loginController');
+const authMiddleware = require('../middleware/authMiddleware');
+const cart = require('../controllers/cartController');
 const router = express.Router();
 
 router.get('/products', getProducts);
+router.post('/login', express.json(), login);
+router.post('/cart/:productId', authMiddleware, cart);
 
 module.exports = router;
