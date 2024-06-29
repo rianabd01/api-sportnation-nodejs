68375def909204ad02e4e0b5ef1b79b0332d9af9
rianabd01 <rianwahyuab@gmail.com>
2024-06-29 22:24:57 +0700

    Add features: helmet and ratelimiter

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const appConfig = require('./app/config/app.conf');
+const helmet = require('helmet');
+const ratelimiter = require('express-rate-limit');
+
+const limiter = ratelimiter({
+  windowMs: 60 * 1000 * 15, // 15 minute
+  max: 100, // 100 request maximal
+  message: 'Youe reach limit request API',
+});
+
+// Middleware
+app.use(limiter);
+app.use(helmet());
 
 // Import routes
-
 const appRoutes = require('./app/routes/App.routes');
 // const authRoutes = require('./app/routes/Auth.routes');
 // const dashboardRoutes = require('./app/routes/Dashboard.routes');
