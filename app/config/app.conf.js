034f16fce5ca7c3d7c64b53b33c407ec747a096d
rianabd01 <rianwahyuab@gmail.com>
2024-06-30 22:39:44 +0700

    Add Features: OTP email service

--- app/config/app.conf.js
+++ app/config/app.conf.js
@@ -4,6 +4,8 @@ module.exports = {
   jwtSecret: process.env.JWT_SECRET || 'your_jwt_secret_key',
   cryptoKey: process.env.CRYPTO_KEY || 'your_crypto_key',
   logLevel: process.env.LOG_LEVEL || 'info',
+  emailService: process.env.EMAIL_SERVICE || 'email',
+  emailPassword: process.env.EMAIL_PASS || 'pass',
   appName: 'SportNation',
   appDescription: 'Buy used sports equipment',
 };
