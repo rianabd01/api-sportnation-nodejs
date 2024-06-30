da0348506b1e56b3921ecddc053fd10823964233
rianabd01 <rianwahyuab@gmail.com>
2024-06-30 22:34:32 +0700

    Add Features: Otp_verification

--- app/routes/App.routes.js
+++ app/routes/App.routes.js
@@ -4,10 +4,12 @@ const login = require('../controllers/Auth/loginController');
 const authMiddleware = require('../middleware/authMiddleware');
 const cart = require('../controllers/cartController');
 const register = require('../controllers/Auth/registerController');
+const otp = require('../controllers/Auth/otpController');
 const router = express.Router();
 
 router.get('/products', getProducts);
 router.post('/register', express.json(), register);
+router.post('/otp', express.json(), otp);
 router.post('/login', express.json(), login);
 router.post('/cart/:productId', authMiddleware, cart);
 
