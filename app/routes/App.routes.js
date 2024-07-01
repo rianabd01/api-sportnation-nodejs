140c2447e1403e7dfdf758a3fcfc6a10078ea7c6
rianabd01 <rianwahyuab@gmail.com>
2024-07-01 22:47:52 +0700

    Add: cart and order features

--- app/routes/App.routes.js
+++ app/routes/App.routes.js
@@ -5,12 +5,18 @@ const authMiddleware = require('../middleware/authMiddleware');
 const cart = require('../controllers/cartController');
 const register = require('../controllers/Auth/registerController');
 const otp = require('../controllers/Auth/otpController');
+const cartItems = require('../controllers/cartItemsController');
+const order = require('../controllers/orderController');
 const router = express.Router();
 
 router.get('/products', getProducts);
 router.post('/register', express.json(), register);
 router.post('/otp', express.json(), otp);
 router.post('/login', express.json(), login);
+// add to cart
 router.post('/cart/:productId', authMiddleware, cart);
+// get cartItem
+router.get('/cart', authMiddleware, cartItems);
+router.post('/order', authMiddleware, express.json(), order);
 
 module.exports = router;
