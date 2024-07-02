e487da7e288be6f868f8daff3471329e7a8494a3
rianabd01 <rianwahyuab@gmail.com>
2024-07-02 22:21:55 +0700

    Fix: split route

--- app/routes/App.routes.js
+++ app/routes/App.routes.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const getProducts = require('../controllers/productController');
-const login = require('../controllers/Auth/loginController');
 const authMiddleware = require('../middleware/authMiddleware');
 const cart = require('../controllers/cartController');
-const register = require('../controllers/Auth/registerController');
-const otp = require('../controllers/Auth/otpController');
+
 const cartItems = require('../controllers/cartItemsController');
-const order = require('../controllers/orderController');
 const router = express.Router();
 
+// get products list
 router.get('/products', getProducts);
-router.post('/register', express.json(), register);
-router.post('/otp', express.json(), otp);
-router.post('/login', express.json(), login);
 // add to cart
 router.post('/cart/:productId', authMiddleware, cart);
 // get cartItem
 router.get('/cart', authMiddleware, cartItems);
-router.post('/order', authMiddleware, express.json(), order);
 
 module.exports = router;
