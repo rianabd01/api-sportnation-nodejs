9a975a7c2b842313ba251bbd839dca3d531046df
rianabd01 <rianwahyuab@gmail.com>
2024-07-02 22:20:14 +0700

    Add features: queri order and price range

--- app/controllers/productController.js
+++ app/controllers/productController.js
@@ -3,12 +3,34 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const getProducts = async (req, res) => {
+  const { sortBy, range } = req.query;
   try {
-    const products = await prisma.product.findMany();
+    const queryOptions = {};
 
+    if (sortBy === 'cheap') {
+      queryOptions.orderBy = { price: 'asc' };
+    }
+
+    if (sortBy === 'expensive') {
+      queryOptions.orderBy = { price: 'desc' };
+    }
+
+    if (range) {
+      const bottomPrice = range.split('-')[0];
+      const topPrice = range.split('-')[1];
+
+      queryOptions.where = {
+        price: {
+          gte: Number(bottomPrice),
+          lte: Number(topPrice),
+        },
+      };
+    }
+
+    const products = await prisma.product.findMany(queryOptions);
     res.status(201).json({ data: products });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).send('Internal server error');
   }
 };
 
